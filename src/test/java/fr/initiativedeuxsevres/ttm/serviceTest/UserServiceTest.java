8b8a45039500b6df391b28c4ca84af7b536fdc8e
corantoine <coralie.antoine@maif.fr>
2024-11-06 11:03:01 +0100

    initialisation des tests

---
+++ src/test/java/fr/initiativedeuxsevres/ttm/serviceTest/UserServiceTest.java
@@ -0,0 +1,14 @@
+package fr.initiativedeuxsevres.ttm.serviceTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class UserServiceTest {
+
+    @Test
+    public void userCreationTest(){
+
+    }
+
+}
