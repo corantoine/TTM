2f1a84ba0aa7ea186a370befcc700c54ba54d012
corantoine <158170889+corantoine@users.noreply.github.com>
2024-11-08 15:38:48 +0100

    ajout du texte de la page d'accueil (#334)

--- src/main/javascript/src/components/Home.js
+++ src/main/javascript/src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import logo_TTM from "../picture/logo_TTM.svg";
 import home from "../styles/home.css"
+import TextAccueil from "./TextAccueil";
 
 const Home = () => {
 return(
@@ -9,12 +10,9 @@ return(
         <img src = {logo_TTM} className="app_logo" alt="logo de l'application"/>
     </div>
     <div className="texte_accueil">
-        {/* {TexteAccueil} */}Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tempus pretium nulla, in efficitur erat. Vestibulum pretium dapibus libero at suscipit. Quisque tempus ipsum enim, vel dapibus odio aliquam porta. Donec suscipit tortor vel iaculis gravida. Mauris malesuada congue bibendum. Phasellus bibendum commodo euismod. Vivamus semper felis a dui porttitor hendrerit. 
-        Suspendisse a pellentesque quam, vitae eleifend velit. Integer aliquet et enim vel aliquet. 
-        Vestibulum sollicitudin, est sit amet imperdiet pellentesque, tortor nisl scelerisque ligula, luctus dignissim nulla enim et quam. 
-        Praesent id ultrices ligula, nec pulvinar ipsum. 
+        <TextAccueil/>
     </div>
-    <button id="home_button">Première connexion ?</button>
+    <button type="submit" id="home_button">Première connexion ?</button>
     </section>
 ) ;
 }
