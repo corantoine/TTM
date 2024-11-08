a23e56c980eed4af616ea79f9098468aa30827fe
corantoine <coralie.antoine@maif.fr>
2024-11-08 15:34:43 +0100

    ajout du texte de la page d'accueil

---
+++ src/main/javascript/src/components/TextAccueil.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const TextAccueil = () => {
+    return (
+        <div>
+            <p>Trouve ton match est une appli qui vous permettra de trouver <strong>LA</strong> </p>
+            <p>personne pour vous accompagner et vous soutenir,</p>
+            <p>une épaule sur laquelle vous reposer.</p> 
+            <br/>
+            <p>Le but étant de créer un climat de confiance avec cette </p>
+            <p>personne afin que vous puissiez vous tourner vers elle</p> 
+            <p>lorsque vous avez des soucis ou des besoins.</p> 
+            <br/>
+            <p>Ce parrain / marraine peut être un chef d’entreprise, ou un</p>
+             <p>cadre dirigeant ayant de l’expérience et un réseau important</p> 
+             <p>pour faire appel à des partenaires experts dans vos besoins.</p>
+        </div>
+    );
+};
+
+export default TextAccueil;
