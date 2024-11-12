ac1908f1d70ba5af253d34c3fd7abf8063921877
corantoine <coralie.antoine@maif.fr>
2024-11-12 12:35:45 +0100

    init footer

--- src/main/javascript/src/components/Footer.js
+++ src/main/javascript/src/components/Footer.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from 'react'
+import "../styles/footer.css"
+import logoFooter from "../picture/LogoFooterTTM.svg"
 
-const Footer = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-};
+function Footer() {
+  return (
+    <div>
+      <footer className="footer">
+      <img src={logoFooter} className='logo-ttm-footer'/>
+      <p>Votre plateforme : Initiative Deux-Sèvres</p>
+      <p>Voici le footer</p>
+      <p>Voici le footer</p>
+      <p>Voici le footer</p>
+      <p>Voici le footer</p>
+      <p>Voici le footer</p>
+      <p>Voici le footer</p>
+      </footer>
+    </div>
+  )
+}
 
-export default Footer;
+export default Footer
