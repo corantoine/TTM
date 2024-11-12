aecddd0994ddd2b5e8b1f04713b9635fd0622a6a
corantoine <158170889+corantoine@users.noreply.github.com>
2024-11-12 16:55:13 +0100

    feat: creation header et footer (#336)
    * debut Layout/Header/Footer
    
    * header ok, pas encore de menu burger, pas encore responsive
    
    * init footer
    
    * footer ok

--- src/main/javascript/src/components/Header.js
+++ src/main/javascript/src/components/Header.js
@@ -1,49 +1,20 @@
-import React from 'react'
-import '../styles/header.css'
-import { useLocation } from 'react-router-dom'
+import React from 'react';
+import '../styles/header.css';
+import logo from '../picture/logo.svg';
+import loginLogo from "../picture/loginLogo.svg"
 
-// const paths = {
-//   uoTitle: '/uo',
-//   roleTitle: '/role',
-//   acteurTitle: '/acteur',
-//   homeTitle: '/',
-// }
-
-// const whichPath = (currentPath) => {
-//   switch (currentPath) {
-//     case paths.uoTitle:
-//       return 'Unites organisationnelles'
-//     case paths.roleTitle:
-//       return 'ROLES'
-//     case paths.acteurTitle:
-//       return 'ACTEURS'
-//     case paths.homeTitle:
-//       return 'Bienvenue sur Flow Chart Tree !'
-//     default:
-//       return console.error("cette page n'existe pas.")
-//   }
-// }
 export default function Header() {
-  // const location = useLocation()
-  // const currentPath = location.pathname
-  // console.log('log de currentPath : ', currentPath)
-
-  // return (
-  //   <nav className="header">
-  //     <h3 className="title-site">
-  //       TTM
-  //     </h3>
-  //     <div>
-  //       <h1 className="header-title">Bienvenue sur TTM !</h1>
-  //     </div>
-  //     <ul className="nav-items">
-  //       <li className="li-nav">
-  //         LOGO
-  //       </li>
-  //       <li className="li-nav">
-  //         CONNEXION
-  //       </li>
-  //     </ul>
-  //   </nav>
-  // )
+  return (
+    <nav className="header">
+      <img src={logo} className='logo-initiative' alt="logo initiative"/>
+      <div className="header-title-container">
+        <h1 className="header-title">Bienvenue sur TTM !</h1>
+      </div>
+      <ul className="nav-items">
+        <li className="li-nav">
+          <img src={loginLogo} className='login-logo' alt='logo de connexion'/>
+        </li>
+      </ul>
+    </nav>
+  );
 }
