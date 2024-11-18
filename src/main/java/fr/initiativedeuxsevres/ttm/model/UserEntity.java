50fad1c9c2a8cc0b0f90c626cd3e4c96f17ba8b4
corantoine <coralie.antoine@maif.fr>
2024-11-18 10:38:49 +0100

    liaison bdd ok + test ok

--- src/main/java/fr/initiativedeuxsevres/ttm/model/UserEntity.java
+++ src/main/java/fr/initiativedeuxsevres/ttm/model/UserEntity.java
@@ -3,12 +3,14 @@ package fr.initiativedeuxsevres.ttm.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Entity
 @Builder
+@Table(name = "users")
 public class UserEntity {
     @Id
     @GeneratedValue
