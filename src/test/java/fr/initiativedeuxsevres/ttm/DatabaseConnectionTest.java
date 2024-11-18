277dada6a66ad74bd446c2e6e66ae412ff1aadaf
corantoine <coralie.antoine@maif.fr>
2024-11-18 10:38:49 +0100

    liaison bdd ok + test ok

---
+++ src/test/java/fr/initiativedeuxsevres/ttm/DatabaseConnectionTest.java
@@ -0,0 +1,31 @@
+package fr.initiativedeuxsevres.ttm;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Classe permettant de tester que l'application est connectée à la base de données.
+ */
+
+@SpringBootTest
+public class DatabaseConnectionTest {
+
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void testConnection() throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+            Assertions.assertNotNull(connection);
+            Assertions.assertFalse(connection.isClosed());
+        }
+    }
+}
