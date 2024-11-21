2dc293da8aacf4af5c67318e246ea740183c22e0
corantoine <coralie.antoine@maif.fr>
2024-11-21 09:39:37 +0100

    fix: bff

--- src/main/javascript/App.js
+++ src/main/javascript/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Layout from "./components/Layout";
+import React from 'react';
 
 function App() {
   return (
--- src/main/javascript/index.js
+++ src/main/javascript/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+export function init(targetId){
+const root = ReactDOM.createRoot(document.getElementById(targetId));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
