8ec24e87f15d9fb076bff2536fa0b67aa7aa8764
corantoine <coralie.antoine@maif.fr>
2024-11-26 11:43:44 +0100

    fix: css propre

--- src/main/javascript/components/Home.js
+++ src/main/javascript/components/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import home from "../styles/home.css"
 import TextAccueil from "./TextAccueil";
 import { LogoTtm } from "../picture/LogoTtm";
+import { Link } from "react-router-dom";
 
 const Home = () => {
 return(
@@ -12,8 +13,8 @@ return(
     <div className="texte_accueil">
         <TextAccueil/>
     </div>
-    <button type="submit" id="home_button">Première connexion ?</button>
-    </section>
+    <Link to= '/login'><button type="submit" id="home_button">Première connexion ?</button>
+    </Link></section>
 ) ;
 }
 
