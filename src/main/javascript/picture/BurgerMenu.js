617ed16cc45011b46019ee372417314c78351e20
corantoine <coralie.antoine@maif.fr>
2024-11-26 16:14:42 +0100

    feat: menu burger coloré inactif"
    git status

---
+++ src/main/javascript/picture/BurgerMenu.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export const BurgerMenu = () => (
+  <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M4 18L20 18"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+    />
+    <path
+      d="M4 12L20 12"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      strokeLinecap="round"
+    />
+    <path
+      d="M4 6L20 6"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      strokeLinecap="round"
+    />
+  </svg>
+)
