da1899f299b2efe5418a4e50f7160cc089f528f7
corantoine <coralie.antoine@maif.fr>
2024-12-05 15:52:26 +0100

    feat(Model): creation classes parrainEntity/porteurEntity

--- src/main/java/fr/initiativedeuxsevres/ttm/model/UserEntity.java
+++ src/main/java/fr/initiativedeuxsevres/ttm/model/UserEntity.java
@@ -3,18 +3,21 @@ package fr.initiativedeuxsevres.ttm.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.Table;
-import lombok.Builder;
 import lombok.Data;
+import lombok.experimental.SuperBuilder;
 
 @Data
-@Entity
-@Builder
-@Table(name = "users")
+@SuperBuilder
+@MappedSuperclass
 public class UserEntity {
     @Id
     @GeneratedValue
     Long id;
+    String username;
     String nom;
     String prenom;
     String plateformeInitiative;
--- src/main/java/fr/initiativedeuxsevres/ttm/service/UserService.java
+++ src/main/java/fr/initiativedeuxsevres/ttm/service/UserService.java
@@ -1,37 +1,37 @@
-package fr.initiativedeuxsevres.ttm.service;
-
-import java.util.NoSuchElementException;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import fr.initiativedeuxsevres.ttm.model.Role;
-import fr.initiativedeuxsevres.ttm.model.UserEntity;
-import fr.initiativedeuxsevres.ttm.repository.UserRepository;
-
-@Service
-public class UserService {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    /**
-     * Crée un nouvel utilisateur et le sauvegarde
-     * @param user L'entité user à créer
-     * @return L'entité user créée et sauvegardée
-     */
-    public UserEntity createUser(UserEntity user) {
-        return userRepository.save(user);
-    }
-
-    public void deleteUserById(Long userId) {
-        Optional<UserEntity> maybeUser = userRepository.findById(userId);
-        if (maybeUser.isPresent()) {
-            userRepository.deleteById(userId);
-            return;
-        }
-        String erreurMessage = String.format("L'utilisateur %d n'existe pas", userId);
-        throw new NoSuchElementException(erreurMessage);
-    }
-}
+//package fr.initiativedeuxsevres.ttm.service;
+//
+//import java.util.NoSuchElementException;
+//import java.util.Optional;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import fr.initiativedeuxsevres.ttm.model.Role;
+//import fr.initiativedeuxsevres.ttm.model.UserEntity;
+//import fr.initiativedeuxsevres.ttm.repository.UserRepository;
+//
+//@Service
+//public class UserService {
+//
+//    @Autowired
+//    private UserRepository userRepository;
+//
+//    /**
+//     * Crée un nouvel utilisateur et le sauvegarde
+//     * @param user L'entité user à créer
+//     * @return L'entité user créée et sauvegardée
+//     */
+//    public UserEntity createUser(UserEntity user) {
+//        return userRepository.save(user);
+//    }
+//
+//    public void deleteUserById(Long userId) {
+//        Optional<UserEntity> maybeUser = userRepository.findById(userId);
+//        if (maybeUser.isPresent()) {
+//            userRepository.deleteById(userId);
+//            return;
+//        }
+//        String erreurMessage = String.format("L'utilisateur %d n'existe pas", userId);
+//        throw new NoSuchElementException(erreurMessage);
+//    }
+//}
