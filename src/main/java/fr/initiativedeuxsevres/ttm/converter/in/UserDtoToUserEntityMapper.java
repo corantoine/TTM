5b77fc966d66f8fded43c5495e0cc73c60a9f895
corantoine <coralie.antoine@maif.fr>
2025-01-02 10:43:49 +0100

    docs(UserDtoToUserEntityMapper): redaction de la javadoc

--- src/main/java/fr/initiativedeuxsevres/ttm/converter/in/UserDtoToUserEntityMapper.java
+++ src/main/java/fr/initiativedeuxsevres/ttm/converter/in/UserDtoToUserEntityMapper.java
@@ -5,6 +5,15 @@ import fr.initiativedeuxsevres.ttm.model.UserEntity;
 
 public class UserDtoToUserEntityMapper {
 
+    /**
+     * Convertit un DTO utilisateur en une entité utilisateur.
+     * Cette méthode prend un objet UserDto en entrée et retourne un objet UserEntity
+     * contenant les mêmes informations. Cela permet de transférer les données utilisateur
+     * de manière sécurisée et structurée entre les différentes couches de l'application.
+     *
+     * @param userDto le DTO utilisateur à convertir
+     * @return un objet UserEntity contenant les informations de l'utilisateur
+     */
     public static UserEntity convertUserDtoToUserEntity(UserDto userDto) {
         return UserEntity.builder()
                 .nom(userDto.getNom())
