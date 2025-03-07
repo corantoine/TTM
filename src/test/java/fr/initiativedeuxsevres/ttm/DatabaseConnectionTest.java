7d50aad5edef58d4e958b3e196dd6c0392b5fd38
corantoine <coralie.antoine@maif.fr>
2025-03-07 13:14:44 +0100

    Update DatabaseConnectionTest.java

--- src/test/java/fr/initiativedeuxsevres/ttm/DatabaseConnectionTest.java
+++ src/test/java/fr/initiativedeuxsevres/ttm/DatabaseConnectionTest.java
@@ -1,33 +1,33 @@
-package fr.initiativedeuxsevres.ttm;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-import javax.sql.DataSource;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-/**
- * Classe permettant de tester que l'application est connectée à la base de données.
- */
-
-@SpringBootTest
-public class DatabaseConnectionTest {
-
-
-    @Autowired
-    private DataSource dataSource;
-
-    @Disabled("")
-    @Test
-    public void testConnection() throws SQLException {
-        try (Connection connection = dataSource.getConnection()) {
-            Assertions.assertNotNull(connection);
-            Assertions.assertFalse(connection.isClosed());
-        }
-    }
-}
+//package fr.initiativedeuxsevres.ttm;
+//
+//import java.sql.Connection;
+//import java.sql.SQLException;
+//
+//import javax.sql.DataSource;
+//
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Disabled;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+///**
+// * Classe permettant de tester que l'application est connectée à la base de données.
+// */
+//
+//@SpringBootTest
+//public class DatabaseConnectionTest {
+//
+//
+//    @Autowired
+//    private DataSource dataSource;
+//
+//    @Disabled("")
+//    @Test
+//    public void testConnection() throws SQLException {
+//        try (Connection connection = dataSource.getConnection()) {
+//            Assertions.assertNotNull(connection);
+//            Assertions.assertFalse(connection.isClosed());
+//        }
+//    }
+//}
